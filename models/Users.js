32af9e0ed4232daa8b5066a1173acce88ea6e6bb
Alex <alexander.kurilyonok@rednavis.com>
2018-03-04 19:20:58 +0300

    made users authentication with saving used.id in the session

--- models/Users.js
+++ models/Users.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
+const findOrCreate = require('mongoose-findorcreate')
 
-const UserSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-});
+
+const UserSchema = new mongoose.Schema(
+    {
+        profileId: Number,
+        profile: Object
+    },
+    {
+        versionKey: false 
+    }
+);
+
+UserSchema.plugin(findOrCreate);
 
 module.exports = mongoose.model('users', UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const mongoose = require('mongoose');
 const Users = require('../models/Users.js');
 
 /* GET ALL Users */
-router.get('/', function(req, res, next) {
-    Users.find(function (err, results) {
-    if (err) return next(err);
-    res.send(results);
-  });
+// router.get('/', function(req, res, next) {
+//     Users.find(function (err, results) {
+//     if (err) return next(err);
+//     res.send(results);
+//   });
+// });
+
+router.get('/', (req,res,next) => {
+  //const userId = req.session.userId;
+  console.log(req.session)
+  res.render('index');
+})
+
+router.get('/auth/yandex',
+  passport.authenticate('yandex'),
+  function(req, res){
+    // The request will be redirected to Yandex for authentication, so this
+    // function will not be called.
 });
 
+router.get('/auth/yandex/callback',
+  passport.authenticate('yandex', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+});
+
+
 module.exports = router;
+
