2148e9427e27e1a6e82b64d90baf027b5481f6fb
ilya <ilya.kudin@rednavis.com>
2018-03-07 18:28:13 +0300

    Refactor passport bootstrap

--- config/passport.js
+++ config/passport.js
@@ -1,44 +1,42 @@
-const passport = require('passport');
 const YandexStrategy = require('passport-yandex').Strategy;
 const Users = require('./../models/Users.js');
 
 
+const haveRednavisEmail = (emails) => {
+    return emails.reduce((valid, email) => {
+        return valid = valid + (email.value.indexOf('@rednavis.com') !== -1);
+    }, 0);
+};
+
+const handleLoggedUser = (profile, done) => {
+    Users.findOneAndUpdate({ profileId: profile.id }, { profile: profile._json }, { new: true , upsert: true },
+        function (err, user) {
+            done(err, user);
+        }
+    );
+};
+
 module.exports = function(passport) {
     //credentials for Yandex
-    const YANDEX_CLIENT_ID = "cd85db83d6914976ba839957733ab2dc"
+    const YANDEX_CLIENT_ID = "cd85db83d6914976ba839957733ab2dc";
     const YANDEX_CLIENT_SECRET = "a8549941e9304ac6982808c2cb1a2b3a";
 
     // Passport session setup.
-    passport.serializeUser(function(user, done) {
-        done(null, user.id);
-    });
+    passport.serializeUser((user, done) => done(null, user.id));
+    passport.deserializeUser((id, done) => Users.findById(id, (err, user) => done(null,user)));
     
-    passport.deserializeUser(function(id, done) {
-        Users.findById(id, function(err,user){
-        err 
-            ? done(err)
-            : done(null,user);
-        });
-    });
-    
-    passport.use(new YandexStrategy({
-                clientID: YANDEX_CLIENT_ID,
-                clientSecret: YANDEX_CLIENT_SECRET,
-                callbackURL: "http://localhost:3000/auth/yandex/callback"
-            },
-            function(accessToken, refreshToken, profile, done) {
-                if(~profile.username.indexOf('@rednavis.com')){
-                    Users.findOneAndUpdate({ profileId: profile.id }, { profile: profile._json },{ new: true , upsert: true } ,function (err, user) {
-                        if (err) return new Error(err);
-                        return done(err, user);
-                        });
-                }
-                else {
-                    console.log('bad connection')
-                }
-                
+    passport.use(
+        new YandexStrategy({
+            clientID: YANDEX_CLIENT_ID,
+            clientSecret: YANDEX_CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/auth/yandex/callback"
+        },
+        (accessToken, refreshToken, profile, done) => {
+            if (haveRednavisEmail(profile.emails)) {
+                handleLoggedUser(profile, done);
+            } else {
+                done(new Error('Not allowed account'));
             }
+        }
     ));
-}
-
-
+};
