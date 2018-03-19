65cd7582013ea387aa3837532735a91c94d85669
Alex <alexander.kurilyonok@rednavis.com>
2018-03-20 02:02:56 +0300

    refactor code | add saveData function

--- models/Dinners.js
+++ models/Dinners.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
 const DinnerSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    energy_falue: String
-});
+    date: { type: Date, default: Date.now },
+    menuList: [
+                {
+                    name: String,
+                    description: String,
+                    energy_value: String
+                }
+              ]
+    },
+    { 
+        versionKey: false 
+    }
+);
 
 module.exports = mongoose.model('menus', DinnerSchema);
